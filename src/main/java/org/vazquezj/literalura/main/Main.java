b7f9e71c153aa26a16facb9b90e576146f9c8516
JosueVJ <josuevj.1e.49@gmail.com>
2024-11-15 13:33:26 -0600

    feat: add opciones method and explain static method differences
    - Added opciones method to Main class
    - Explained the difference between private static and public static methods

--- src/main/java/org/vazquezj/literalura/main/Main.java
+++ src/main/java/org/vazquezj/literalura/main/Main.java
@@ -2,11 +2,62 @@ package org.vazquezj.literalura.main;
 
 import org.vazquezj.literalura.service.APIFetcher;
 
+import java.net.URLEncoder;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+	private Scanner scanner = new Scanner(System.in);
 	private APIFetcher fetchAPI = new APIFetcher();
 
+	// Constantes
+	private static final String URL_BASE = "https://gutendex.com/books/?search=";
+
 	public void menu() {
-		var json = fetchAPI.obtenerDatos("https://gutendex.com/books/?search=pride");
-		System.out.printf("JSON: %s\n", json);
+		String opcion;
+		do {
+			mostrarMenu();
+			opcion = scanner.nextLine();  // Leemos toda la línea y eliminamos espacios en blanco.
+
+			switch (opcion) {
+				case "1":
+					buscarLibro();
+					break;
+				case "0":
+					System.out.println("Cerrando la aplicación...");
+					break;
+				default:
+					System.out.println("Opción inválida, por favor intente de nuevo.");
+			}
+		} while (!opcion.equals("0"));
+	}
+
+	private void mostrarMenu() {
+		String menu = """
+                *************************************************************
+                *                                                           *
+                *       BIENVENIDO A LA APLICACIÓN DE LITERATURA            *
+                *                                                           *
+                *************************************************************
+                
+                1 - Buscar libro (se guarda en la base de datos si no existe aún)
+                0 - Salir
+                
+                Por favor, ingrese el número de la opción deseada: """;
+		System.out.println(menu);
+	}
+
+	private void buscarLibro() {
+		System.out.println("Ingrese el nombre del libro que desea buscar:");
+		String tituloLibro = scanner.nextLine();  // Obtenemos el nombre del libro
+		if (!tituloLibro.isEmpty()) {
+			// Codificamos el nombre del libro para usarlo en la URL
+			String encoded = URLEncoder.encode(tituloLibro, java.nio.charset.StandardCharsets.UTF_8);
+			String url = URL_BASE + encoded;
+			String json = fetchAPI.obtenerDatos(url);  // Llamamos al API
+			System.out.println(json);  // Mostramos la respuesta
+		} else {
+			System.out.println("No ha ingresado ningún título. Intente de nuevo.");
+		}
 	}
 }
--- src/main/java/org/vazquezj/literalura/service/APIFetcher.java
+++ src/main/java/org/vazquezj/literalura/service/APIFetcher.java
@@ -8,21 +8,23 @@ import java.net.http.HttpResponse;
 
 public class APIFetcher {
 	public String obtenerDatos(String url) {
+		//cliente que se encarga de hacer la peticion
 		HttpClient client = HttpClient.newHttpClient();
+
+		//peticion que se va a realizar
 		HttpRequest request = HttpRequest.newBuilder()
 				.uri(URI.create(url))
 				.build();
+
+		//respuesta que se obtiene de la peticion
 		HttpResponse<String> response = null;
-		try {
-			response = client
-					.send(request, HttpResponse.BodyHandlers.ofString());
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} catch (InterruptedException e) {
+
+		//lanzamos la peticion
+		try{
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			return response.body();
+		} catch (IOException | InterruptedException e) {
 			throw new RuntimeException(e);
 		}
-
-		String json = response.body();
-		return json;
 	}
 }
